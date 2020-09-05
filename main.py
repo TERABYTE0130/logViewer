cd99d37bc3178969cf56f1fbc8bf71086326c582
shunsuke_terauchi <kwgcr368@gmail.com>
2020-09-05 20:08:20 +0900

    add log

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
+import os.path
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtUiTools import QUiLoader
 
-class UISample(QtWidgets.QMainWindow):
+CURRENT_PATH = os.path.dirname(os.path.join(os.path.abspath(sys.argv[0])))
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self,parent = None):
-        super(UISample, self).__init__(parent)
-        self.resize(400,300)
+        super(MainWindow, self).__init__(parent)
+        self.ui = QUiLoader().load(os.path.join(CURRENT_PATH, "window", "log_window.ui"))
+        self.setCentralWidget(self.ui)
+        self.ui.DebugLogPushButton.clicked.connect(self.clickLogPushButton)
+
+    def clickLogPushButton(self):
+        text = self.ui.DebugInputText.toPlainText()
+        self.ui.LogView.append(text)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = UISample()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
