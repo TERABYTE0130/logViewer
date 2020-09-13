d5f27c6f8dcba8777b2a347a162c12b66796ffb9
shunsuke_terauchi <kwgcr368@gmail.com>
2020-09-13 23:25:17 +0900

    send responce

--- log_server.py
+++ log_server.py
@@ -1,4 +1,5 @@
-from PySide2.QtCore import (Signal, QByteArray, QDataStream, QIODevice, QThread, Qt)
+from PySide2.QtCore import (Signal, QByteArray, QDataStream, QIODevice, QThread, QByteArray,
+                            QDataStream, QIODevice, Qt)
 from PySide2.QtNetwork import (QHostAddress, QNetworkInterface, QTcpServer, QTcpSocket)
 
 
@@ -9,18 +10,30 @@ class LogThread(QThread):
         super(LogThread,self).__init__(parent)
         self.socket_desc = socket_desc
 
-    def returnResponce(msg:str)
-
+    def createResponce(self, msg: str):
+        write_block = QByteArray()
+        output_buf = QDataStream(write_block, QIODevice.WriteOnly)
+        output_buf.setVersion(QDataStream.Qt_5_15)
+        #output_buf.setVersion(QDataStream.Qt_4_0)
+        output_buf.writeUInt16(0)
+        output_buf.writeQString(msg)
+        output_buf.device().seek(0)
+        output_buf.writeUInt16(write_block.size() - 2)
+        return write_block
 
     def run(self):
         tcp_socket = QTcpSocket()
-        #socket生成
+        #socket生成と
         if not tcp_socket.setSocketDescriptor(self.socket_desc):
             self.error.emit(tcp_socket.error())
             print("faild create socket")
             return
         print("connected!!")
+        connect_responce = self.createResponce("connect log server!!")
+        tcp_socket.write(connect_responce)
 
+        tcp_socket.disconnectFromHost()
+        tcp_socket.waitForDisconnected()
 
 class Server(QTcpServer):
     def incomingConnection(self, socket_desc):
