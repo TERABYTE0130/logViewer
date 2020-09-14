fa1c7e498f7a8dba5201206e1cdaaf45cfce1447
shunsuke_terauchi <kwgcr368@gmail.com>
2020-09-15 02:04:57 +0900

    connect signal slot

--- log_server.py
+++ log_server.py
@@ -1,44 +1,53 @@
-from PySide2.QtCore import (Signal, QByteArray, QDataStream, QIODevice, QThread, QByteArray,
+from PySide2.QtCore import (QObject, Signal, QByteArray, QDataStream, QIODevice, QThread, QByteArray,
                             QDataStream, QIODevice, Qt)
 from PySide2.QtNetwork import (QHostAddress, QNetworkInterface, QTcpServer, QTcpSocket)
 
+class StringSignal(QObject):
+    logging_signal = Signal((str,))
 
 class LogThread(QThread):
     error = Signal(QTcpSocket.SocketError)
 
-    def __init__(self, socket_desc, parent):
-        super(LogThread,self).__init__(parent)
+    def __init__(self, socket_desc, signal, parent):
+        super(LogThread, self).__init__(parent)
         self.socket_desc = socket_desc
+        self.signal = signal
+        self.log_signal = StringSignal()
 
     def createResponce(self, msg: str):
         write_block = QByteArray()
         output_buf = QDataStream(write_block, QIODevice.WriteOnly)
         output_buf.setVersion(QDataStream.Qt_5_15)
-        #output_buf.setVersion(QDataStream.Qt_4_0)
         output_buf.writeUInt16(0)
         output_buf.writeQString(msg)
         output_buf.device().seek(0)
         output_buf.writeUInt16(write_block.size() - 2)
         return write_block
 
+    def setLogSlot(self, slot_func):
+        self.log_signal.logging_signal[str].connect(slot_func)
+
     def run(self):
         tcp_socket = QTcpSocket()
-        #socket生成と
+        # socket生成
         if not tcp_socket.setSocketDescriptor(self.socket_desc):
             self.error.emit(tcp_socket.error())
             print("faild create socket")
             return
-        print("connected!!")
         connect_responce = self.createResponce("connect log server!!")
         tcp_socket.write(connect_responce)
-
+        self.log_signal.logging_signal[str].emit("send message")
         tcp_socket.disconnectFromHost()
         tcp_socket.waitForDisconnected()
 
 class Server(QTcpServer):
+    def __init__(self, log_slot):
+        super(Server, self).__init__()
+        self.log_slot = log_slot
+
     def incomingConnection(self, socket_desc):
-        print("connect begin")
-        thread = LogThread(socket_desc, self)
+        thread = LogThread(socket_desc, self.log_slot, self)
+        thread.setLogSlot(self.log_slot)
         # 終了をトリガーして破棄する
         thread.finished.connect(thread.deleteLater)
         thread.start()
--- main.py
+++ main.py
@@ -4,24 +4,22 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtWidgets import QMessageBox
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtNetwork import (QHostAddress, QNetworkInterface)
+from PySide2.QtCore import (Signal, Slot)
 import log_server
-
-
 
 CURRENT_PATH = os.path.dirname(os.path.join(os.path.abspath(sys.argv[0])))
 SERVER_IP = QHostAddress(QHostAddress.LocalHost)
 SERVER_PORT = 10001
 
+
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        #load window
+        # load window
         self.ui = QUiLoader().load(os.path.join(CURRENT_PATH, "window", "log_window.ui"))
         self.setCentralWidget(self.ui)
-        #self.ui.DebugLogPushButton.clicked.connect(self.clickLogPushButton)
-
-        #init server
-        self.server = log_server.Server()
+        # init server
+        self.server = log_server.Server(self.connected)
 
     def startLogServer(self, address, port):
         print("begin listen ", (address.toString()))
@@ -29,6 +27,11 @@ class MainWindow(QtWidgets.QMainWindow):
             QMessageBox.critical(self, "logserver", "unable to start server")
             self.close()
             return
+
+    # @QtCore.Slot(str)
+    def connected(self, msg):
+        print(msg)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
