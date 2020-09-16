92a5b6210ec1f2cb8b4bf91f0ec71db18338aa64
shunsuke_terauchi <kwgcr368@gmail.com>
2020-09-16 21:23:42 +0900

    受信できない状態

--- log_server.py
+++ log_server.py
@@ -2,8 +2,10 @@ from PySide2.QtCore import (QObject, Signal, QByteArray, QDataStream, QIODevice,
                             QDataStream, QIODevice, Qt)
 from PySide2.QtNetwork import (QHostAddress, QNetworkInterface, QTcpServer, QTcpSocket)
 
+
 class StringSignal(QObject):
     logging_signal = Signal(str)
+
 
 class LogThread(QThread):
     error = Signal(QTcpSocket.SocketError)
@@ -13,6 +15,7 @@ class LogThread(QThread):
         self.socket_desc = socket_desc
         self.signal = signal
         self.log_signal = StringSignal()
+        self.is_connect_client = True
 
     def createResponce(self, msg: str):
         write_block = QByteArray()
@@ -24,6 +27,9 @@ class LogThread(QThread):
         output_buf.writeUInt16(write_block.size() - 2)
         return write_block
 
+    def sendToClient(self, socket: QTcpSocket, buf):
+        socket.write(buf)
+
     def setLogSlot(self, slot_func):
         self.log_signal.logging_signal[str].connect(slot_func)
 
@@ -34,11 +40,32 @@ class LogThread(QThread):
             self.error.emit(tcp_socket.error())
             print("faild create socket")
             return
+        connect_response = self.createResponce("test")
+        self.sendToClient(tcp_socket,connect_response)
+        while self.is_connect_client:
+            while tcp_socket.bytesAvailable() < 2:
+                print("wait next log ...")
+                if not tcp_socket.waitForReadyRead(-1):
+                    print("failed recv")
+                print("loop... ")
+
+            read_buf = QDataStream(tcp_socket)
+            block_size = read_buf.readUInt16()
+            print(block_size)
+            while tcp_socket.bytesAvailable() < block_size:
+                if not tcp_socket.waitForReadyRead(-1):
+                    print(tcp_socket.errorString())
+
+            recv_log = read_buf.readQString()
+            print("success recv log")
+            print(recv_log)
+
         connect_responce = self.createResponce("connect log server!!")
         tcp_socket.write(connect_responce)
         self.log_signal.logging_signal[str].emit("send message")
         tcp_socket.disconnectFromHost()
         tcp_socket.waitForDisconnected()
+
 
 class Server(QTcpServer):
     def __init__(self, log_slot):
@@ -51,3 +78,4 @@ class Server(QTcpServer):
         # 終了をトリガーして破棄する
         thread.finished.connect(thread.deleteLater)
         thread.start()
+       
